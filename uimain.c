ddcc6d4006e38671302442f6403a23b7af1b3d43
student <zgomez4@miners.utep.edu>
2021-09-12 20:14:24 -0600

    Added arrow method and new font to project

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a,New font n, Chars = c) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -28,6 +28,15 @@ int main()
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      break;
+    case 'a':
+      puts("You selected arrow:");
+      print_arrow();
+      break;
+    case 'n':
+      puts("You selected new font:");
+      for (char c = 'A'; c < 'G'; c++)
+	print_char_5x7(c);
       break;
     case 'q':
       puts("Bye!");
